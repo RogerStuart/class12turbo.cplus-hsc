714bead6d257564a83d3cb19bffee1f2a63c52df
RogerStuart <stuartfernandes31@gmail.com>
2024-12-14 21:58:01 +0530

    Update sum of n natural no.s
    This is a program to find sum of 'n' (input given by user ) 
    eg. user input---> so 6+5+4+3+2+1=21
    will be the result given as output

--- sum of n natural no.s
+++ sum of n natural no.s
@@ -2,11 +2,21 @@
 #include <conio.h>
 void main(){
 clrscr();
-int i;
+
+int i=1;
 int n;
+int sum=0;
+
 cout<<"Enter the value to get 1st 'n' numbers"<<endl; 
 cin>>n;
+
 while(i<=n)
 {sum=sum+i;
-i++}
+i++;}
+
+cout<<sum<<endl;
+
 getch();
+}
+
+
